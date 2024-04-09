351e0c1542d0c64f9469083f12d8afc0e99cad4b
Maria Castro <mafecastro0705@gmail.com>
2024-04-09 14:44:30 -0500

    0.2

---
+++ programa.py
@@ -0,0 +1,16 @@
+#importan la biblioteca 
+import numpy as np
+
+#una forma de crear arreglos ndarray es usando una lista
+
+miLista=[3,5,7,9]
+miArreglo=np.array(miLista)
+
+
+#Dimensiones 
+print(miArreglo.ndim) 
+print(miArreglo.shape)
+print(miArreglo.size)
+print(miArreglo.dtype)
+
+miArreglo2=np.array([3,6,7,90])
