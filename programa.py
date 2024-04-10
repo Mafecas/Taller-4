eb1e808cd59be9006cf3824a0d869b48053eca31
Maria Castro <mafecastro0705@gmail.com>
2024-04-10 15:45:22 -0500

    explicacion

--- programa.py
+++ programa.py
@@ -1,10 +1,16 @@
+#importamos la biblioteca Numpy y le agregamos el alias 
 import numpy as np
+#Impotamos la biblioteca de matplotlib para crear graficas y le agregamos el alias 
 import matplotlib.pyplot as plt
+#definimos la funcion de matplot y las 3 variables con las que se van a trabajar
 def mandelbrot(h, w, maxit=20, r=2):
-    """Returns an image of the Mandelbrot fractal of size (h,w)."""
+#creamos un rango de datos que queramos en este caso es -2.5 a 1.5 y genera una lista entre ese rango con un espasci0 definido 4*h+1
     x = np.linspace(-2.5, 1.5, 4*h+1)
+#creamos un rango de datos que queramos en este caso es -1.5 a 1.5 y genera una lista entre ese rango con un espasci0 definido 3*w+1
     y = np.linspace(-1.5, 1.5, 3*w+1)
+#Asignacion multiple, A y B y se le asigna a x y Y 
     A, B = np.meshgrid(x, y)
+    #se realiza una operacion aritmetica, y C es la nueva variable a la que se va asignar 
     C = A + B*1j
     z = np.zeros_like(C)
     divtime = maxit + np.zeros(z.shape, dtype=int)
